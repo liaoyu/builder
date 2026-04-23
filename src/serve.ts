845e97edb9f4fe78f4de2fe69d2fe905dbda8930
liaoxiaoyou <liaoxiaoyou@qiniu.com>
2026-04-23 19:07:28 +0800

    chore: 升级 swc 依赖

--- src/serve.ts
+++ src/serve.ts
@@ -4,6 +4,7 @@
  */
 
 import fs from 'fs'
+import os from 'os'
 import url from 'url'
 import webpack from 'webpack'
 import WebpackDevServer from 'webpack-dev-server'
@@ -84,7 +85,17 @@ async function runDevServer(port: number) {
   })
 
   server.listen(port, host, () => {
-    logger.info(`Server started on ${colors.cyan(`http://localhost:${port}`)}`)
+    const localUrl = `http://localhost:${port}`
+    const networkUrls = getNetworkUrls(port)
+    const arrow = colors.green('➜')
+    const messages = [
+      `${arrow}  ${colors.bold('Local:')}   ${colors.cyan(localUrl)}`,
+      ...networkUrls.map(networkUrl => (
+        `${arrow}  ${colors.bold('Network:')} ${colors.cyan(networkUrl)}`
+      ))
+    ]
+
+    logger.info(`Server started:\n\n${messages.join('\n')}`)
   })
 
   return () => new Promise<void>(resolve => {
@@ -153,4 +164,18 @@ function getHistoryApiFallbackRewrites(buildConfig: BuildConfig) {
       )
     })
   )
+}
+
+function getNetworkUrls(port: number) {
+  const addresses = new Set<string>()
+
+  Object.values(os.networkInterfaces()).forEach(networkInterface => {
+    networkInterface?.forEach(address => {
+      if (address.family === 'IPv4' && !address.internal) {
+        addresses.add(`http://${address.address}:${port}`)
+      }
+    })
+  })
+
+  return [...addresses]
 }
